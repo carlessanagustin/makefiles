91f9d911ab3e9ec9aa6fe5b93405ae0ca4e3e6a5
Carles San Agustin <carles.sanagustin@scrm.eu>
2020-01-31 10:49:23 +0100

    updated aks_upgrade*

--- Makefile_azure.mk
+++ Makefile_azure.mk
@@ -5,3 +5,13 @@ aks_context:
 # Create a container registry
 acr_create:
 	az acr create --name $(ACR_NAME)  --subscription $(AKS_SUBS) --resource-group $(AKS_RG) --sku Basic --location $(LOCATION) --admin-enabled=true --tags $(TAGS)
+
+# upgrading aks
+aks_upgrade_list:
+	az aks get-upgrades --subscription $(AKS_SUBS) --resource-group $(AKS_RG) --name $(AKS_NAME) --output table
+
+aks_upgrade_start:
+	az aks upgrade --subscription $(AKS_SUBS) --resource-group $(AKS_RG) --name $(AKS_NAME) --kubernetes-version $(AKS_VERSION)
+
+aks_upgrade_status:
+	az aks show --subscription $(AKS_SUBS) --resource-group $(AKS_RG) --name $(AKS_NAME) --output table
